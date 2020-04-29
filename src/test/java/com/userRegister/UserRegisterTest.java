d70c75da049d4897394ab21b1ca7843170bf6fa7
kyoolbaba <kyoolbaba@gmail.com>
2020-04-29 07:14:56 +0530

    Added all the negative test  cases

--- src/test/java/com/userRegister/UserRegisterTest.java
+++ src/test/java/com/userRegister/UserRegisterTest.java
@@ -7,33 +7,57 @@ import org.junit.Test;
 public class UserRegisterTest {
 private UserRegister userRegister;
     @Test
-    public void firstNameTesting() {
+    public void positiveFirstNameTesting() {
     boolean condition=userRegister.checkFirstName("Milan");
         Assert.assertTrue(condition);
     }
-
     @Test
-    public void lastNameTesting() {
+    public void negativeFirstNameTesting() {
+        boolean condition=userRegister.checkFirstName("milan");
+        Assert.assertFalse(condition);
+    }
+    @Test
+    public void positiveLastNameTesting() {
         boolean condition=userRegister.checkLastName("Gowda");
         Assert.assertTrue(condition);
     }
+    @Test
+    public void negativeLastNameTesting() {
+        boolean condition=userRegister.checkLastName("gowda");
+        Assert.assertFalse(condition);
+    }
 
     @Test
-    public void validateEmailTesting() {
+    public void positiveValidateEmailTesting() {
         boolean condition=userRegister.checkEmailID("milan.gowda.adb@gmail.co.in");
         Assert.assertTrue(condition);
     }
-
     @Test
-    public void validateMobileTesting() {
-    boolean condition=userRegister.validateMobile("07760987606");
-    Assert.assertTrue(condition);
+    public void negativeValidateEmailTesting() {
+        boolean condition=userRegister.checkEmailID("@gmail.co.in");
+        Assert.assertFalse(condition);
     }
 
     @Test
-    public void validatePassword() {
+    public void positiveValidateMobileTesting() {
+    boolean condition=userRegister.validateMobile("07760987606");
+    Assert.assertTrue(condition);
+    }
+    @Test
+    public void negativeValidateMobileTesting() {
+        boolean condition=userRegister.validateMobile("987606");
+        Assert.assertFalse(condition);
+    }
+
+    @Test
+    public void positiveValidatePassword() {
     boolean condition=userRegister.validatePassword("mil$anGowdasjs0");
     Assert.assertTrue(condition);
+    }
+    @Test
+    public void negativeValidatePassword() {
+        boolean condition=userRegister.validatePassword("milanGowdasjs0");
+        Assert.assertFalse(condition);
     }
 
     @Before
